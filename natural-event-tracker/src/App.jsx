f4c9e22f6fac1648dba09019524a3962bf53c0b8
Krisma Antonio <krismapaulene.antonio681@myci.csuci.edu>
2024-12-18 19:44:34 -0800

    import mapbox

--- natural-event-tracker/src/App.jsx
+++ natural-event-tracker/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import Map from './components/Map';
 
 function App() {
 
   return (
-    
-      <div>
-        <h1>Natural Event Tracker</h1>
-      </div>
-
-  );
+    <Map/>
+  )
 }
 
 export default App
---
+++ natural-event-tracker/src/components/Map.jsx
@@ -0,0 +1,15 @@
+import { useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const Map = () => {
+
+    return (
+        <>
+          <div id='map-container' />
+        </>
+    )
+    
+}
+
+export default Map
