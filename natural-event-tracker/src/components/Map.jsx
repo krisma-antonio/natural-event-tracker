7d27c496762981a0597ae24ff30e2735243cd0c6
Krisma Antonio <krismapaulene.antonio681@myci.csuci.edu>
2024-12-19 17:54:37 -0800

    removed closebutton in popup

--- natural-event-tracker/src/components/Map.jsx
+++ natural-event-tracker/src/components/Map.jsx
@@ -7,7 +7,7 @@ const Map = ({eventData}) => {
   const mapRef = useRef()
   const mapContainerRef = useRef()
 
-  const NATURAL_EVENT = "severeStorms";
+  const NATURAL_EVENT = "wildfires";
 
   useEffect(() => {
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
@@ -17,35 +17,36 @@ const Map = ({eventData}) => {
   
   const markers = eventData.map((ev, index) => {
     if(ev.categories[0].id === NATURAL_EVENT) {
-      console.log("Yes work");
+      console.log("Yes work!");
       new mapboxgl.Marker()
             .setLngLat([ ev.geometry[0].coordinates[0], ev.geometry[0].coordinates[1] ])
             .setPopup(
-              new mapboxgl.Popup({ offset: 25 }) // add popups
+              new mapboxgl.Popup({ offset: 25, className: "pop-up", closeOnClick: true, closeButton: false }) // add popups
                 .setHTML(
                   `<h3>${ev.title}</h3><p>${ev.description}</p>`
                 )
             )
             .addTo(mapRef.current);
     } else {
+      console.log("No woork!");
       return null;
     }
   })
   
   markers
 
-    return () => {
-      mapRef.current.remove()
- 
-    }
+  return () => {
+    mapRef.current.remove()
 
-  }, [])
+  }
 
-  return (
-    <>
-      <div id='map-container' ref={mapContainerRef}/>
-    </>
-  )
+}, [])
+
+return (
+  <>
+    <div id='map-container' ref={mapContainerRef}/>
+  </>
+)
     
 }
 
