801964b5975b1c4b583b190cb0eb76ea3e8b84b6
Krisma Antonio <krismapaulene.antonio681@myci.csuci.edu>
2024-12-19 17:01:11 -0800

    added mapbox and nasa api(practice)

--- natural-event-tracker/src/App.jsx
+++ natural-event-tracker/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Map from './components/Map';
 
 function App() {
+  const [eventData, setEventData] = useState([])
+  const urlNasa = 'https://eonet.gsfc.nasa.gov/api/v3/events?limit=10&status=open&api_key=' + import.meta.env.VITE_NASA_API_KEY;
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const res = await fetch(urlNasa)
+      const { events } = await res.json()
+
+      setEventData(events)
+    }
+
+    fetchEvents()
+  }, [])
+
+  console.log(eventData);
 
   return (
-    <Map/>
+    <Map eventData={eventData}/>
   )
 }
 
--- natural-event-tracker/src/components/Map.jsx
+++ natural-event-tracker/src/components/Map.jsx
@@ -1,14 +1,51 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const Map = () => {
+const Map = ({eventData}) => {
 
-    return (
-        <>
-          <div id='map-container' />
-        </>
-    )
+  const mapRef = useRef()
+  const mapContainerRef = useRef()
+
+  const NATURAL_EVENT = "severeStorms";
+
+  useEffect(() => {
+    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+    });
+  
+  const markers = eventData.map((ev, index) => {
+    if(ev.categories[0].id === NATURAL_EVENT) {
+      console.log("Yes work");
+      new mapboxgl.Marker()
+            .setLngLat([ ev.geometry[0].coordinates[0], ev.geometry[0].coordinates[1] ])
+            .setPopup(
+              new mapboxgl.Popup({ offset: 25 }) // add popups
+                .setHTML(
+                  `<h3>${ev.title}</h3><p>${ev.description}</p>`
+                )
+            )
+            .addTo(mapRef.current);
+    } else {
+      return null;
+    }
+  })
+  
+  markers
+
+    return () => {
+      mapRef.current.remove()
+ 
+    }
+
+  }, [])
+
+  return (
+    <>
+      <div id='map-container' ref={mapContainerRef}/>
+    </>
+  )
     
 }
 
