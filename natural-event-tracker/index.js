3ffb4f2b3d67a3b1b606782e73fec1eb1fa9fe0f
Krisma Antonio <krismapaulene.antonio681@myci.csuci.edu>
2025-04-07 18:51:15 -0700

    added index.js for backend

--- natural-event-tracker/index.js
+++ natural-event-tracker/index.js
@@ -5,4 +5,7 @@ const cors = require(cors)
 const axios = require(axios)
 require('dotenv').config()
 
-const app = express()
+const app = express() 
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
